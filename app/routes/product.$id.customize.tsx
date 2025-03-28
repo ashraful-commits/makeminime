a2db6164c6a5b297862b49b59f821bb76780f714
Md Ashraful alam <ashrafulalam10000@gmail.com>
2025-03-28 12:24:13 +0600

    error handleer

--- app/routes/product.$id.customize.tsx
+++ app/routes/product.$id.customize.tsx
@@ -341,7 +341,7 @@ export default function ProductIdCustomize() {
 
   const handleConfirmExit = (confirm: boolean) => {
     if (confirm) {
-      navigate("/");
+      navigate("https://makeminime.com/");
     } else {
       setShowPopup(false);
     }
